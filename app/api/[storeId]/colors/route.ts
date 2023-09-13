d323af77c552f54248db1874fb221032b88bafb7
Chellappan <vrchellappan@gmail.com>
2023-09-13 22:42:26 +0530

    Add Colors API

---
+++ app/api/[storeId]/colors/route.ts
@@ -0,0 +1,71 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("StoreId is required", { status: 401 });
+    }
+
+    const sizes = await prismadb.color.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(sizes);
+  } catch (error) {
+    console.log(`[COLORS_GET]`, JSON.stringify(error));
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { name, value } = body;
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+    if (!name) {
+      return new NextResponse("Name is required", { status: 401 });
+    }
+    if (!value) {
+      return new NextResponse("Value is required", { status: 401 });
+    }
+    if (!params.storeId) {
+      return new NextResponse("StoreId is required", { status: 401 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const size = await prismadb.color.create({
+      data: {
+        name,
+        value,
+        storeId: params.storeId,
+      },
+    });
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log(`[COLORS_POST]`, JSON.stringify(error));
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
